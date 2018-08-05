b9eb7406cf3e69a04fe6c61b8ad3282490db4387
mzlikesb <mintzzzzz42@gmail.com>
2018-08-05 21:41:52 +0900

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/List.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // 연결 정보를 저장하는 Node 클래스
+    class Node<T>
+    {
+        public T info;
+        public Node<T> link;
+
+        public Node(T i)
+        {
+            info = i;
+            link = null;
+        }
+    }
+
+    // 단일 연결 리스트 클래스
+    class SingleLinkedList<T> : IComparable
+    {
+        private Node<T> start;                      // head 역할
+        private Node<T> end;                        // tail 역할 
+
+        public SingleLinkedList()
+        {
+            start = null;
+            end = null;
+        }             // 리스트 기본값을 만드는 함수
+
+        public Node<T> GetStart()
+        {
+            return start;
+        }                // Start를 찾아오는 함수
+
+        public T Get(int x)
+        {
+            Node<T> temp = start;
+            for (int i = 0; i < x; i++)
+            {
+                temp = temp.link;
+            }
+            return temp.info;
+        }                 // 노드의 정보를 가져오는 함수
+
+
+        public Node<T> GetNode(int x)
+        {
+            Node<T> temp = start;
+            for (int i = 0; i < x; i++)
+            {
+                temp = temp.link;
+            }
+            return temp;
+        }            // 해당 번호의 노드를 가져오는 함수
+
+        public void Add(T Data)
+        {
+            if (start == null)
+            {
+                start = new Node<T>(Data);
+                end = start;
+            }
+            else
+            {
+                Node<T> A = new Node<T>(Data);
+                end.link = A;
+                end = A;
+            }
+
+        }                // 노드를 맨 뒤에 추가하는 함수
+
+        public void Add(T Data, int x)         // 노드를 중간에 추가하는 함수
+        {
+            Node<T> temp1 = GetNode(x - 1).link;
+            GetNode(x - 1).link = new Node<T>(Data);
+            GetNode(x).link = temp1;
+        }
+
+        public void Del(int x)
+        {
+            if (Length() == x + 1)
+            {
+                end = GetNode(x - 1);
+                GetNode(x - 1).link = null;
+            }
+            else
+            {
+                GetNode(x - 1).link = GetNode(x + 1);
+            }
+
+        }                // 인덱스 값을 넣어서 지우는 함수
+
+        public void Del(Node<T> y)
+        {
+            if (start == y)
+            {
+                start = start.link;     // 스타트가 이제 기존의 스타트가 가리키던 얘를 가리킨다.
+            }
+            else
+            {
+                Node<T> temp1 = start;
+                Node<T> temp2 = start.link;
+                while (temp2 != y)
+                {
+                    temp1 = temp2;
+                    temp2 = temp2.link;
+                }
+                temp1.link = temp2.link;
+            }
+        }               // 노드를 직접 넣어서 지우는 함수
+
+        public int Length()
+        {
+            int count = 0;
+            Node<T> temp = start;
+            while (temp != end)
+            {
+                count += 1;
+                temp = temp.link;
+            }
+            return count + 1;          // 마지막 위치의 NODE 포함
+        }                    // 리스트의 길이를 구해주는 함수
+
+        public void Show(int x)
+        {
+            Console.WriteLine(Get(x));
+        }                // 해당 번호의 노드에 담긴 정보를 출력해주는 함수
+
+        public int CompareTo(object obj)
+        {
+            return this.CompareTo(obj);
+        }
+
+        public T this[int x]
+        {
+            get { return Get(x); }
+            set
+            {
+                Node<T> temp = start;
+                for (int i = 0; i < x; i++)
+                {
+                    temp = temp.link;
+                }
+                temp.info = value;
+            }
+
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Registration.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Registration
+    {//수강신청 처리 클래스
+        
+        //	학생 정보 초기화
+        public int CreateStudentList(StudentInfo[] studentList)
+        {
+            int i = 0;
+
+	        for(i = 0; i < Program.MAX_STUDENT_CNT; i++)
+	        {
+                if (i < Program.MIN_STUDENT_CNT)
+			        studentList[i].id = 1001 + i;
+		        else
+			        studentList[i].id = -1;
+                studentList[i].classList = new SingleLinkedList<ClassInfo>();
+	        }
+            //기본 20명의 학생정보 출력
+            Console.WriteLine("기등록 된 " + Program.MIN_STUDENT_CNT + "명의 학생 목록");
+
+            DisplayAllInfo(studentList);
+
+	        return i;
+        }
+        
+        //	콘솔 메뉴 화면
+        public void DisplayMenu()
+        {
+	        Console.WriteLine("========================================================");
+	        Console.WriteLine("1. 모든 학생정보 출력. ");
+	        Console.WriteLine("2. 학생 등록. ");
+	        Console.WriteLine("3. 과목 추가. ");
+	        Console.WriteLine("4. 과목 삭제. ");
+	        Console.WriteLine("5. Exit program. ");
+            Console.WriteLine("========================================================");
+            Console.Write("선택 : ");
+        }
+        
+        //	기능부
+        public int RunFunction(int n, StudentInfo[] studentList)
+        {
+	        switch(n)
+	        {
+	        case 1 :
+                DisplayAllInfo(studentList);
+		        break;
+	        case 2 :
+                AddStudent(studentList);
+		        break;
+	        case 3 :
+                AddClass(studentList);
+		        break;
+	        case 4:
+                DropClass(studentList);
+		        break;
+	        case 5 :
+		        Console.WriteLine("Exit program.");
+		        return -1;
+	        default :
+                Console.WriteLine("잘못된 선택입니다.");
+		        return 1;
+	        }
+
+	        return 0;
+        }
+        //	케이스 1 모든 학생정보 출력
+        private void DisplayAllInfo(StudentInfo[] studentList)
+        {
+	        int i = 0;
+	       
+	        Console.WriteLine("========================================================");
+	        Console.WriteLine("  번호  |  ID  |  과목 목록(과목명, 분반, 학점)");
+            Console.WriteLine("========================================================");
+
+	        for(i = 0; i < Program.MAX_STUDENT_CNT; i++)
+	        {
+		        if(studentList[i].id == -1)
+			        continue;
+
+                Console.Write("  {0,3}  | {1,4} |", i + 1, studentList[i].id);
+
+                foreach (ClassInfo ci in studentList[i].classList)
+                {
+                    Console.Write("  (" + ci.code + ", " + ci.section.ToString() + ", " +
+                        ci.credit.ToString() + ")");
+                }
+
+                Console.WriteLine("");
+	        }
+            Console.WriteLine("========================================================\n");
+        }
+
+        
+        // 학생 등록
+        private void AddStudent(StudentInfo[] studentList)
+        {
+	        int stuID;
+	        int i = 0;
+
+	        // 빈공간 체크
+	        for(i = 0; i < Program.MAX_STUDENT_CNT; i++)
+	        {
+		        if(studentList[i].id == -1)
+		        {
+			        break;
+		        }
+	        }
+
+	        if(i == Program.MAX_STUDENT_CNT)
+	        {
+		        Console.WriteLine("더 등록할 수 없습니다.\n");
+		        return;
+	        }
+
+            // 등록시에 학생 ID 입력
+            Console.Write("학번을 입력하세요 : ");
+            try
+            {
+                stuID = int.Parse(Console.ReadLine());
+                if (stuID > 1999 || stuID < 1)
+                {
+                    Console.WriteLine("Error!! 1 에서 1999까지의 숫자를 입력하세요.");
+                    return;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error!! 1 에서 1999까지의 숫자를 입력하세요.");
+                return;
+            }
+        	
+            // 학생 등록
+	        studentList[i].id = stuID;
+            Console.WriteLine(stuID.ToString() + " 추가되었습니다.\n");
+        }
+        
+        //	과목 등록
+        private void AddClass(StudentInfo[] studentList)
+        {
+	        int stuID = 0;
+	        int index = -1;
+	        int i = 0;
+	        ClassInfo tempInfo;
+        	
+            // 과목 정보 등록할 특정 학생 ID 입력
+	        Console.Write("학번을 입력하세요 : ");
+	        try
+            {
+                stuID = int.Parse(Console.ReadLine());
+                if (stuID > 1999 || stuID < 1)
+                {
+                    Console.WriteLine("Error!! 1 에서 1999까지의 숫자를 입력하세요.");
+                    return;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error!! 1 에서 1999까지의 숫자를 입력하세요.");
+                return;
+            }
+
+	        for(i = 0; i < Program.MAX_STUDENT_CNT; i++)
+	        {
+		        if(studentList[i].id == stuID)
+		        {
+			        index = i;
+			        break;
+		        }
+	        }
+
+	        if(index == -1)
+	        {
+		        Console.WriteLine("해당 학생이 존재하지 않습니다.\n");
+		        return;
+	        }
+        	
+	        tempInfo = new ClassInfo();
+
+            Console.Write("과목명 입력 : ");
+            tempInfo.code = Console.ReadLine();
+	            		
+	        Console.Write("분반 입력 : ");            
+            try
+            {
+                tempInfo.section = int.Parse(Console.ReadLine());               
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error! 정수만 입력가능.");
+                return;
+            }
+
+	        Console.Write("학점 입력 : ");
+            try
+            {
+                tempInfo.credit = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error! 정수만 입력가능.");
+                return;
+            }
+
+            // 학생정보에 과목등록
+            studentList[index].classList.Add(tempInfo);
+
+	        Console.WriteLine("학번 : " + studentList[index].id.ToString() + ", 추가됨 (" +
+                tempInfo.code + ", " + 
+		        tempInfo.section.ToString() + ", " + tempInfo.credit.ToString() + ")\n");
+        }
+        
+        //	수강포기
+        private void DropClass(StudentInfo[] studentList)
+        {
+	        int stuID = 0;
+	        int index = -1;
+	        int i = 0;
+            string dropCode;
+	        int dropSection;
+
+            Console.Write("학번을 입력하세요 : ");
+            try
+            {
+                
+                stuID = int.Parse(Console.ReadLine());
+                if (stuID > 1999 || stuID < 1)
+                {
+                    Console.WriteLine("Error!! 1 에서 1999까지의 숫자를 입력하세요.");
+                    return;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error!! 1 에서 1999까지의 숫자를 입력하세요.");
+                return;
+            }
+
+            // 해당학생 검색
+	        for(i = 0; i < Program.MAX_STUDENT_CNT; i++)
+	        {
+		        if(studentList[i].id == stuID)
+		        {
+			        index = i;
+			        break;
+		        }
+	        }
+
+	        if(index == -1)
+	        {
+                Console.WriteLine("해당 학생이 존재하지 않습니다.\n");
+                return;
+	        }
+            //수강과목이 없을경우
+	        if(studentList[index].classList.Count == 0)
+	        {
+                Console.WriteLine("현재 수강하고 있는 과목이 없습니다.\n");
+		        return;
+	        }
+
+            // 현재 수강하고 있는 과목 목록 출력
+            Console.WriteLine("현재 수강하고 있는 과목 목록");
+	        Console.WriteLine("-------------------------------------------------");
+	        Console.WriteLine(" 과목명              분반              학점 ");
+            Console.WriteLine("-------------------------------------------------");
+
+            i = 0;
+            foreach (ClassInfo ci in studentList[index].classList)
+            {
+                Console.WriteLine(" {0}. {1,-10}{2,12}{3,20}",i, ci.code, ci.section, ci.credit);
+            }
+            Console.WriteLine("-------------------------------------------------");
+
+            // 드랍할 과목 정보 입력
+            Console.WriteLine("어떤과목을 삭제할건가요? ");
+	        Console.Write("삭제할 과목명 입력 : ");
+
+            dropCode = Console.ReadLine();
+	                	
+	        Console.Write("삭제할 과목의 분반 입력: ");
+            try
+            {
+                dropSection = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error! 정수만 입력가능.\n");
+                return;
+            }
+
+            for(i = 0; i < studentList[index].classList.Count; i++)
+            {
+                if(studentList[index].classList[i].code == dropCode &&
+                    studentList[index].classList[i].section == dropSection)
+                {
+                    Console.WriteLine("과목명 : " + studentList[index].classList[i].code + ", 분반 : " + 
+                         studentList[index].classList[i].section.ToString() + " 과목이 삭제되었습니다.\n");
+                    
+                    studentList[index].classList.RemoveAt(i);                    
+                    return;
+                }
+            }
+
+            Console.WriteLine("Error! 찾을 수 없습니다.\n");            
+        }
+    }
+}
